81b7396c4732d7ea636ded0a2c2b419fb2714c8f
omomerong <omomerong@gmail.com>
2024-12-08 01:57:57 +0900

    feat: 다크모드 추가

--- src/screens/Homescreen.tsx
+++ src/screens/Homescreen.tsx
@@ -1,25 +1,40 @@
 import React, { useState } from 'react'
-import { SafeAreaView } from 'react-native'
-import Streak from '../components/Streak'
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native'
 import CharacterImg from '../components/CharacterImg'
 import styled from 'styled-components/native'
-
-// TODO: streak 전역 변수 관리
+import StreakTracker from './StreakTracker'
 
 function Homescreen() {
-  const [dayStreak, setDayStreak] = useState(0)
+  const theme = useColorScheme()
+  const isDarkTheme = theme === 'dark'
+
+  const year2YY = new Date().getFullYear().toString().slice(-2)
+  const month2MM = (new Date().getMonth() + 1).toString().padStart(2, '0')
+  const day2DD = new Date().getDate().toString().padStart(2, '0')
   return (
-    <SafeAreaView>
-      <ImageBox style={{ backgroundColor: '#FFF' }}>
-        <Title>
-          <Streak dayStreak={dayStreak} />
-        </Title>
-        <CharacterImg />
-        <Title>🏃</Title>
-        <Title>💡</Title>
-        <Title>💻</Title>
-        <Title>👥</Title>
-      </ImageBox>
+    <SafeAreaView style={{ backgroundColor: isDarkTheme ? 'black' : 'white' }}>
+      <Title
+        isDarkTheme={isDarkTheme}
+      >{`${year2YY}.${month2MM}.${day2DD} 🔥1`}</Title>
+      <ScrollView>
+        <ImageBox>
+          <CharacterImg />
+          <StreakTracker />
+          <View style={{ flexDirection: 'row' }}>
+            {/* <Title>🏃</Title>
+          <Title>💡</Title>
+          <Title>💻</Title>
+          <Title>👥</Title>
+          <Title>💵</Title> */}
+          </View>
+        </ImageBox>
+      </ScrollView>
     </SafeAreaView>
   )
 }
@@ -29,12 +44,15 @@ export default Homescreen
 const ImageBox = styled.View`
   width: 100%;
   height: 100%;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   padding: 10px;
 `
-const Title = styled.Text`
+const Title = styled.Text<{ isDarkTheme: boolean }>`
+  text-align: center;
   font-family: 'MinSans-Bold';
   font-weight: bold;
   font-size: 35px;
+  color: ${(props) => (props.isDarkTheme ? 'white' : 'black')};
 `
