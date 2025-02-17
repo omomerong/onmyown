97483f0117b1e98f745fb3e0b1d3b46210d6f56c
omomerong <omomerong@gmail.com>
2025-02-17 16:33:22 +0900

    style: safeareaview 다크모드 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { SafeAreaView } from 'react-native'
+import { SafeAreaView, useColorScheme } from 'react-native'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import RootNavigation from './navigation/RootNavigation'
 // createClient
 const queryClient = new QueryClient()
 
 const App = () => {
+  const theme = useColorScheme()
+  const isDarkTheme = theme === 'dark'
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: isDarkTheme ? 'black' : 'white' }}
+    >
       <QueryClientProvider client={queryClient}>
         <RootNavigation />
       </QueryClientProvider>
